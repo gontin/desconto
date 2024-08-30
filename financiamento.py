31e19a237553d3121f9fdfd55399f968c0a8214f
Gustavo Silveira Nicoletti <gus.nicoletti@gmail.com>
2024-08-30 18:06:38 -0300

    asjhdhbguy

--- financiamento.py
+++ financiamento.py
@@ -1,6 +1,7 @@
 valor = float(input("insira o valor: "))
 qparcelas = float(input("insira quantas parcelas: "))
 juros = float(input("insira o juros: "))
+juros = juros/100
 
 parcela = (valor/qparcelas) + ((valor/qparcelas)*(juros))
 
--- juros.py
+++ juros.py
@@ -1,6 +1,8 @@
 emprestimo = float(input("insira o emprestimo: "))
 juros = float(input("insira o juros: "))
 tempo = float(input("insira quantas vezes o juros foi aplicado: "))
+juros = juros/100
+
 
 valor = emprestimo + ((juros*emprestimo)*tempo)
 
